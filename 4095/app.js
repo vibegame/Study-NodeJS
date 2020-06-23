e798a7dc60264a317b95b691b323344ced14e177
Nikita <46563672+V1be-Official@users.noreply.github.com>
2020-06-23 23:44:54 +0300

    Update app.js

--- 4095/app.js
+++ 4095/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const fetch = require('node-fetch');
+const fetch = require('isomorphic-fetch');
 
 // parse application/json
 app.use(bodyParser.json({ extended: true }));
