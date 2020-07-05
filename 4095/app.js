18b30658b65b0c2d05b8e0d52513f7268e078a8f
Nikita <46563672+V1be-Official@users.noreply.github.com>
2020-07-05 23:12:54 +0300

    update 4095

--- 4095/app.js
+++ 4095/app.js
@@ -3,7 +3,67 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const path = require('path');
+const fs = require('fs');
+
 const fetch = require('isomorphic-fetch');
+
+const databaseFile = path.join(__dirname, 'data.json');
+
+Object.compare = function (obj1, obj2) {
+	//Loop through properties in object 1
+	for (var p in obj1) {
+		//Check property exists on both objects
+		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+ 
+		switch (typeof (obj1[p])) {
+			//Deep compare objects
+			case 'object':
+				if (!Object.compare(obj1[p], obj2[p])) return false;
+				break;
+			//Compare function code
+			case 'function':
+				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+				break;
+			//Compare values
+			default:
+				if (obj1[p] != obj2[p]) return false;
+		}
+	}
+ 
+	//Check object 2 for any extra properties
+	for (var p in obj2) {
+		if (typeof (obj1[p]) == 'undefined') return false;
+	}
+	return true;
+};
+
+const get = () => {
+    let data = '';
+
+    try {
+        data = fs.readFileSync(databaseFile, "utf8");
+    } 
+    catch (error) {
+        if (error.code === 'ENOENT') {
+            fs.writeFile(databaseFile, "", function (err) {
+                if (err) throw err;
+                console.log('File is created successfully.');
+              });
+        } 
+        else {
+            throw error;
+        }
+    }
+    return data ? JSON.parse(data) : {};
+};
+
+const database = get();
+
+const update = () => {
+    fs.writeFileSync(databaseFile, JSON.stringify(database));
+};
+
 
 // parse application/json
 app.use(bodyParser.json({ extended: true }));
@@ -83,6 +143,28 @@ app.post('/proxy', async (req, res) => {
 
         const response = await sendRequest(req.body.method, req.body);
 
+        database.configures.push();
+
+        let hasConfigure = false;
+
+        const newConfigure = {
+            ...req.body
+        };
+
+        for(let i = 0; i < database.configures.length; i++) {
+            const configure = database.configures[i];
+            if(Object.compare(configure, newConfigure)) { 
+                hasConfigure = true;
+                break;
+            }
+        }
+
+        if(!hasConfigure) {
+            database.configures.push(newConfigure);
+            update();
+        }
+        
+
         const stringBody = await getStringBodyFromResponse(response.body);
 
         const headers = {};
@@ -101,6 +183,10 @@ app.post('/proxy', async (req, res) => {
         console.warn(error);
         res.send(error);
     }
+});
+
+app.get('/configures', (req, res) => {
+    res.send(database.configures);
 });
 
 app.listen(port, () => {
