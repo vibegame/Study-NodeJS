c1653a6761551ed0462fc3dd4895bd368eb66be1
Nikita <46563672+V1be-Official@users.noreply.github.com>
2020-07-08 00:33:31 +0300

    ф

--- server1/app.js
+++ server1/app.js
@@ -1,9 +1,17 @@
-const port = 443;
+const port = 1001;
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 app.listen(port, () => {
     console.warn(`App is running. Port: ${port}`);
