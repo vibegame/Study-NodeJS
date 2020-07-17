72d0df816e3ff9609ff64b5162e304fb1cd59309
Nikita <46563672+V1be-Official@users.noreply.github.com>
2020-07-17 14:31:15 +0300

    s

--- server1/app.js
+++ server1/app.js
@@ -5,14 +5,14 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
   })
+});
 
 app.listen(port, () => {
-    console.warn(`App is running. Port: ${port}`);
+  console.warn(`App is running. Port: ${port}`);
 });
